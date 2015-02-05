50d4b7cd89b6365c4eabd9758febdb39da68de34
Jan Kadlec <jan.kadlec@nic.cz>
2015-02-05 16:59:56 +0100

    zone-api: updates to zone-reader API.

--- src/knot/updates/zone-read.c
+++ src/knot/updates/zone-read.c
@@ -15,13 +15,15 @@
  */
 
 #include "knot/updates/zone-read.h"
+#include "libknot/internal/namedb/namedb.h"
+#include "libknot/internal/namedb/namedb_trie.h"
 
 #warning handle empty zones
 
 int zone_read_begin(zone_read_t *r, const knot_zonedb_t *db, const knot_dname_t *qname)
 {
 	r->zone = knot_zonedb_find(db, qname);
-	int ret = zone_lock_init(&r->lock, LOCK_RCU);
+	int ret = zone_lock_init(&r->lock, ZLOCK_RCU);
 	if (ret != KNOT_EOK) {
 		return ret;
 	}
@@ -33,7 +35,7 @@ int zone_read_begin(zone_read_t *r, const knot_zonedb_t *db, const knot_dname_t 
 int zone_read_begin_suffix(zone_read_t *r, knot_zonedb_t *db, const knot_dname_t *qname)
 {
 	r->zone = knot_zonedb_find_suffix(db, qname);
-	int ret = zone_lock_init(&r->lock, LOCK_RCU);
+	int ret = zone_lock_init(&r->lock, ZLOCK_RCU);
 	if (ret != KNOT_EOK) {
 		return ret;
 	}
@@ -52,8 +54,68 @@ const zone_node_t *zone_read_node_for_type(zone_read_t *zr, const knot_dname_t *
 	return zone_contents_find_node_for_type(zr->zone->contents, owner, type);
 }
 
-const zone_node_t *zone_read_find_apex(zone_read_t *zr)
+const zone_node_t *zone_read_apex(zone_read_t *zr)
 {
 	return (const zone_node_t *)zr->zone->contents->apex;
 }
+
+const zone_node_t *zone_read_previous_for_type(zone_read_t *zr, const knot_dname_t *owner, const uint16_t type)
+{
+	zone_contents_find_previous_for_type(zr->zone->contents, owner, type);
+}
+
+const zone_node_t *zone_read_closest_encloser(zone_read_t *zr, const knot_dname_t *owner)
+{
+	zone_contents_find_closest_encloser(zr->zone->contents, owner);
+}
+
+int zone_read_rr_iter(zone_rr_iter_t *it, const zone_read_t *zr, const bool sorted)
+{
+	memset(&it->ch, 0, sizeof(it->ch));
+	it->ch.add = zr->zone->contents;
+	int ret = changeset_iter_add(&it->it, &it->ch, sorted);
+	if (ret != KNOT_EOK) {
+		return ret;
+	}
+	knot_rrset_init_empty(&it->rr);
+
+	return KNOT_EOK;
+}
+
+int zone_read_node_iter(zone_node_iter_t *it, const zone_read_t *zr, const bool sorted, const bool nsec3)
+{
+	zone_tree_t *t = nsec3 ? zr->zone->contents->nsec3_nodes : zr->zone->contents->nodes;
+	if (t == NULL) {
+		return KNOT_EINVAL;
+	}
+
+	namedb_trie_api()->txn_begin(t, &it->txn, NAMEDB_RDONLY);
+	it->it = namedb_trie_api()->iter_begin(&it->txn, sorted ? NAMEDB_SORTED : 0);
+	if (it->it == NULL) {
+		return KNOT_ENOMEM;
+	}
+
+	return KNOT_EOK;
+}
+
+const zone_node_t *zone_read_iter_next_node(zone_node_iter_t *it)
+{
+	if (it && it->it) {
+		if (namedb_trie_api()->iter_next(it->it)) {
+			namedb_val_t val;
+			namedb_trie_api()->iter_val(it->it, &val);
+			return val.data;
+		}
+	}
+
+	return NULL;
+}
+
+const knot_rrset_t *zone_read_iter_next_rr(zone_rr_iter_t *it)
+{
+	it->rr = changeset_iter_next(&it->it);
+	return &it->rr;
+}
+
+void zone_read_node_iter_clear(zone_node_iter_t *it);
 
--- src/knot/updates/zone-read.h
+++ src/knot/updates/zone-read.h
@@ -35,11 +35,34 @@ typedef struct {
 	zone_lock_t lock;
 } zone_read_t;
 
+typedef struct {
+#warning redo
+	changeset_t ch;
+	changeset_iter_t it;
+	knot_rrset_t rr;
+} zone_rr_iter_t;
+
+typedef struct {
+	namedb_txn_t txn;
+	namedb_iter_t *it;
+} zone_node_iter_t;
+
 int zone_read_begin(zone_read_t *r, const knot_zonedb_t *db, const knot_dname_t *qname);
 int zone_read_begin_suffix(zone_read_t *r, knot_zonedb_t *db, const knot_dname_t *qname);
 void zone_read_done(zone_read_t *r);
 
 const zone_node_t *zone_read_node_for_type(zone_read_t *zr, const knot_dname_t *owner, const uint16_t type);
-const zone_node_t *zone_read_get_apex(zone_read_t *zr);
+const zone_node_t *zone_read_apex(zone_read_t *zr);
+const zone_node_t *zone_read_previous_for_type(zone_read_t *zr, const knot_dname_t *owner, const uint16_t type);
+const zone_node_t *zone_read_closest_encloser(zone_read_t *zr, const knot_dname_t *owner);
+
+int zone_read_rr_iter(zone_rr_iter_t *it, const zone_read_t *zr, const bool sorted);
+int zone_read_node_iter(zone_node_iter_t *it, const zone_read_t *zr, const bool sorted, const bool nsec3);
+
+const zone_node_t *zone_read_iter_next_node(zone_node_iter_t *it);
+const knot_rrset_t *zone_read_iter_next_rr(zone_rr_iter_t *it);
+
+void zone_read_node_iter_clear(zone_node_iter_t *it);
+void zone_read_rr_iter_clear(zone_rr_iter_t *it);
 
 /*! @} */
